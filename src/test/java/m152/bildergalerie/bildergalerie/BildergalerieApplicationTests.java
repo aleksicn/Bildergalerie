674fb8c6b3028a202d1f249817202a0c035f7ca1
Nikola Aleksic <aleksicn@bzz.ch>
2022-03-03 14:25:58 +0100

    initial commit

---
+++ src/test/java/m152/bildergalerie/bildergalerie/BildergalerieApplicationTests.java
@@ -0,0 +1,13 @@
+package m152.bildergalerie.bildergalerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BildergalerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
